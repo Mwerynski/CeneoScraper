360f532f568e4b26ba494898c99183c77b028fff
MWerynski <s223543@student.uek.krakow.pl>
2022-04-03 17:12:04 +0200

    adding correct code

--- scraper.py
+++ scraper.py
@@ -1,13 +1,8 @@
-from __future__ import print_function
-from re import sub
+import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import re
-def striphtml(data):
-    p = re.compile(r'<.*?>')
-    return p.sub('', data)
 
-urlpage=urlopen("https://www.ceneo.pl/104942408#tab=reviews").read()
-bswebpage=BeautifulSoup(urlpage)
-results=str(bswebpage.findAll("div",{'class':"user-post__text"}))
-print(striphtml(results))
+
+url = "https://www.ceneo.pl/95365253#tab=reviews"
+response = requests.get(url)
+
+page_dom = BeautifulSoup(response.text, 'html.parser')
